5e5414085ada6874147582ef5e8cc78983c08fcc
stephan281094 <stephan281094@gmail.com>
2015-04-24 23:02:20 +0200

    Clean up and created meteor project for easier prototyping

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var t = new Trianglify({
-    x_gradient: ["yellowgreen","#a1d99b","#31a354"],
-    noiseIntensity: 0
-});
-var pattern = t.generate(
-    document.body.clientWidth,
-    document.body.clientHeight
-);
-document.getElementById('cover').setAttribute('style', 'background-image: '+pattern.dataUrl);
